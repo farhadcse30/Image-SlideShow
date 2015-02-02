8ef3de7e0eb38e8dbf013b421773560ad3eaf559
Farhad <farhadcse30@gmail.com>
2015-02-02 21:34:08 +0600

    SlideShow

---
+++ Assets/Script/SlideShow.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlideShow : MonoBehaviour {
+
+	public float frameInterval = 0.9f;
+	public Texture2D[] imageArray;
+	private int imageIndex = 0;
+	
+	private void Awake() 
+	{
+		if (imageArray.Length >= 1)
+			StartCoroutine( FuncSlideShow() );					
+	}
+	
+	private IEnumerator FuncSlideShow()
+	{
+		while( true )
+		{
+			ChangeImage();
+			yield return new WaitForSeconds(frameInterval);				
+		}		
+	}
+	
+	private void ChangeImage()
+	{
+		imageIndex++;
+		imageIndex = imageIndex % imageArray.Length;
+		Texture2D nextImage = imageArray[ imageIndex ];
+		renderer.material.SetTexture("_MainTex", nextImage);
+	}
+}
